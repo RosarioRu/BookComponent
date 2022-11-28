cffd721abbc3d616af6aed6ed32e7ce7f40d5061
RosarioRu <mariarruval@gmail.com>
2022-11-28 10:17:35 -0800

    Add initial code for the BookControl class.

---
+++ src/components/BookControl.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import NewBookForm from './NewBookForm';
+import BookList from './BookList';
+import Book from './Book';
+
+class BookControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default BookControl;
