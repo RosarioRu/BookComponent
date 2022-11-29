c04358f97774580c26ff1ec1c4466bfad70a33cc
RosarioRu <mariarruval@gmail.com>
2022-11-28 19:51:30 -0800

    Add actual form for adding a new book and function that will get triggered when the form is submitted.

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -1,9 +1,34 @@
 import React from "react";
 
 function NewBookForm(props){
+
+  function handleNewBookFormSubmission(event)
+    {
+      event.preventDefault();
+      console.log(event.target.title.value);
+      console.log(event.target.author.value);
+      console.log(event.target.summary.value);
+    }
+
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewBookFormSubmission}>
+        <input
+          type='text'
+          name='title'
+          placeholder='Book title' 
+        />
+        <input
+          type="text"
+          name="author"
+          placeholder="Author"
+        />
+        <input
+          name="summary"
+          placeholder="brief summary of book"
+        />
+        <button type='submit'>Add book</button>
+      </form>
     </React.Fragment>
   );
 }
