63b9218bf111509afd1a7cdaafdb73c4dd9d9ee1
RosarioRu <mariarruval@gmail.com>
2022-11-28 19:52:24 -0800

    Add method/function that will toggle between showing the form to add a new book, and showing the list of books.

--- src/components/BookControl.js
+++ src/components/BookControl.js
@@ -12,19 +12,42 @@ class BookControl extends React.Component {
     };
   }
 
+  // handleClick = () => {
+  //   this.setState(
+  //     {
+  //       formVisibleOnPage: true
+  //     }
+  //   );
+  // }
+
+  // handleClick = () => {
+  //   this.setState({formVisibleOnPage: true});
+  // }
+
+  //this handleClick method allows toggling! 
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
+
   render() {
     
     let currentlyVisibleState = null;
+    let buttonText = null;  
 
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewBookForm />
+      currentlyVisibleState = <NewBookForm />;
+      buttonText = "Return to Book List";
     } else {
-      currentlyVisibleState = <BookList />
+      currentlyVisibleState = <BookList />;
+      buttonText = "Add a Book";
     }
 
     return(
       <React.Fragment>
         {currentlyVisibleState}
+        <button onClick = {this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
