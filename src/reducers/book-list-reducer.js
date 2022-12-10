c44c21545f6b4a92ba6057ffd5ce7f9f52d75e3c
RosarioRu <mariarruval@gmail.com>
2022-12-10 14:10:50 -0800

    pass second test.

--- src/reducers/book-list-reducer.js
+++ src/reducers/book-list-reducer.js
@@ -1,5 +1,24 @@
 const reducer = (state = {}, action) => {
-  return state;
+
+  // const { title, author, summary, id } = action;
+
+  switch (action.type) {
+  case 'ADD_TICKET': 
+    return Object.assign({}, state, {
+      [action.id]: {
+        // title: title,
+        // author: author,
+        // summary: summary,
+        // id: id
+        title: action.title,
+        author: action.author,
+        summary: action.summary,
+        id: action.id
+      }
+    });
+  default:
+    return state;
+  }
 
 };
 
