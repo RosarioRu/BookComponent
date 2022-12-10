4516686f434d113edf10c1c75927e9d8d75483a8
RosarioRu <mariarruval@gmail.com>
2022-12-10 13:35:50 -0800

    Write line in book list reducer function that returns default state to ensure first test passes.

--- src/__tests__/reducers/book-list-reducer.test.js
+++ src/__tests__/reducers/book-list-reducer.test.js
@@ -2,7 +2,7 @@ import bookListReducer from "../../reducers/book-list-reducer";
 
 
 describe('bookListReducer', () => {
-  test("Should return default state if there is not action type passed into the reducer", () => {
+  test("Should return default state if there is no action type passed into the reducer", () => {
     expect(bookListReducer({}, {type: null})).toEqual({});
   });
 });
