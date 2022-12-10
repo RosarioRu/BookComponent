60a03b0f5e2b201c18cff87fce41921c0149ecec
RosarioRu <mariarruval@gmail.com>
2022-12-10 13:56:56 -0800

    Add second test and ensure it fails.

--- src/__tests__/reducers/book-list-reducer.test.js
+++ src/__tests__/reducers/book-list-reducer.test.js
@@ -2,7 +2,36 @@ import bookListReducer from "../../reducers/book-list-reducer";
 
 
 describe('bookListReducer', () => {
+
+  let action;
+  const bookData = { 
+    title: "Goodnight Moon",
+    author: "Margaret W.",
+    summary: "A bunny says goodnight to everything in his bedroom.",
+    id: 1
+  };
+
   test("Should return default state if there is no action type passed into the reducer", () => {
     expect(bookListReducer({}, {type: null})).toEqual({});
   });
+
+  test("Should sucessfully add a new 'book' to mainBookList with the bookData provided", () => {
+    const { title, author, summary, id } = bookData;
+    action = {
+      type: "ADD_TICKET",
+      title: title,
+      author: author, 
+      summary: summary,
+      id: id
+    };
+    expect(bookListReducer({}, action)).toEqual({
+      [id]: {
+        title: title,
+        author: author,
+        summary: summary,
+        id: id
+      }
+    });
+  });
+
 });
