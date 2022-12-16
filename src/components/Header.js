498230e1b7ae0b374417e02d42736edae8d33af0
RosarioRu <mariarruval@gmail.com>
2022-12-16 13:00:25 -0800

    WIP: some more and alternate styling.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,12 @@ import readImage from './../img/read.png';
 function Header(){
 
   const headerStyles = {
-    border: "1px solid black",
+    // border: "1px solid black",
     marginTop: "0%",
     paddingLeft: "2%",
     paddingRight: "2%",
     paddingTop: "2%",
-    paddingBottom: "2%",
+    paddingBottom: "1%",
     fontFamily: "'apple chancery', 'cursive'",
     textAlign: "left",
     // backgroundImage:`url(${readImage})`,
@@ -17,13 +17,15 @@ function Header(){
     backgroundSize: "contain",
     backgroundRepeat: "no-repeat",
     backgroundPosition: "right",
+    // backgroundPosition: "left",
   
     a: {
       // border: "1px solid black",
       // fontSize: "large",
       display: "inline",
       padding: "3px",
-      paddingLeft: "10px",
+      // paddingLeft: "7%",
+      paddingLeft: "3px",
       paddingRight: "2px",
       fontFamily: "serif",
       fontSize: "small"
