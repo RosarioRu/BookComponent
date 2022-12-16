ca7171f689a4bd25b14993bad2a93befeff030af
RosarioRu <mariarruval@gmail.com>
2022-12-15 21:46:53 -0800

    Remove redux.

--- src/index.js
+++ src/index.js
@@ -2,22 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import { createStore } from 'redux';
-import reducer from './reducers/book-list-reducer';
-import { Provider} from 'react-redux';
-
-const store = createStore(reducer);
-
-store.subscribe(() =>
-console.log(store.getState())
-);
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store = {store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
