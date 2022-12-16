38eebe6f6acd8df47cafc615cfa4d155faca276c
RosarioRu <mariarruval@gmail.com>
2022-12-16 10:39:53 -0800

    Remove v4 id from form.

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {v4} from 'uuid';
 import ReusableForm from "./ReusableForm";
 
 function NewBookForm(props){
@@ -12,7 +11,6 @@ function NewBookForm(props){
         title: event.target.title.value,
         author: event.target.author.value,
         summary: event.target.summary.value,
-        id: v4()
       });
     }
 
@@ -22,23 +20,6 @@ function NewBookForm(props){
         formSubmissionHandler={handleNewBookFormSubmission} 
         buttonText="Add Book"
       />
-      {/* <form onSubmit={handleNewBookFormSubmission}>
-        <input required
-          type='text'
-          name='title'
-          placeholder='Book title' 
-        />
-        <input required
-          type="text"
-          name="author"
-          placeholder="Author"
-        />
-        <input required
-          name="summary"
-          placeholder="brief summary of book"
-        />
-        <button type='submit'>Add book</button>
-      </form> */}
     </React.Fragment>
   );
 }
