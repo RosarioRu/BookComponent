3007b721fb4f65ec551021d2fd72b2ef47f612c6
RosarioRu <mariarruval@gmail.com>
2022-12-16 18:15:28 -0800

    Add log in and sign out functionality to signIn component.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { auth } from "./../firebase.js";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 
 function SignIn() {
 
-  const [signUpSucess, setSignUpSuccess] = useState(null);
-  
+  const [signUpSuccess, setSignUpSuccess] = useState(null);
+  const [signInSuccess, setSignInSuccess] = useState(null);
+  const [signOutSuccess, setSignOutSuccess] = useState(null);
+
   function doSignUp(event) {
-  
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
@@ -20,27 +21,63 @@ function SignIn() {
       .catch((error) => {
         setSignUpSuccess(`There was an error signing up: ${error.message}!`)
       });
-
       updateProfile(auth.currentUser, {
         displayName: userEnteredName
       }).then(() => {
         //profile updated
         setSignUpSuccess(`Welcome, ${auth.currentUser.displayName}, you are now a curator for Little Readers!`)
       });
-    
   }
 
+  function doSignIn(event) {
+    event.preventDefault();
+    const email = event.target.signinEmail.value;
+    const password = event.target.signinPassword.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setSignInSuccess(`Welcome, ${auth.currentUser.displayName}`)
+      })
+      .catch((error) => {
+        setSignInSuccess(`There was an error signing in: ${error.message}`)
+      });
+    setSignOutSuccess(null);
+    }
+
+  function doSignOut() {
+    signOut(auth)
+      .then(function() {
+        setSignOutSuccess("Signed out!");
+      }).catch(function(error) {
+        setSignOutSuccess(`There was an error signing out: ${error.message}!`);
+      });
+  }
 
   return (
     <React.Fragment>
       <h1>Create Your Account</h1>
-      {signUpSucess}
+      {signUpSuccess}
       <form onSubmit={doSignUp}>
         <input type="text" name="email" placeholder="Email address" required />
         <input type="password" name="password" placeholder="Password" required  />
         <input type="text" name="usersname" placeholder="Desired user name" required  />
         <button type="submit">Register</button>
       </form>
+
+      <h1>Sign In</h1>
+      {signInSuccess}
+      <form onSubmit={doSignIn}>
+        <input type="text" name="signinEmail" placeholder="Email" />
+        <input type="password" name="signinPassword" placeholder="Password" />
+        <button type="submit">Log In</button>
+      </form>
+
+      
+      {/* {signOutSuccess ? !null : (<div><h1>Sign Out</h1> <button onClick={doSignOut}>Log Out</button></div>)} */}
+
+      <br/>
+      {signOutSuccess ? null : <button onClick={doSignOut}>Log Out</button>}
+      {signOutSuccess}
+
     </React.Fragment>
   );
 }
